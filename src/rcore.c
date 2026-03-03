53d298330b78aefa92fd2a634dbfbf65f9b10dc7
David <dt052901@gmail.com>
2026-03-02 20:58:01 -0600

    Revert "fix: reset rotation rects in CloseWindow to prevent state poisoning"
    This reverts commit fc935750f92f8b93099597e8d0dbbf7d89b1f9f8.

--- src/rcore.c
+++ src/rcore.c
@@ -770,10 +770,6 @@ void CloseWindow(void)
     ClosePlatform();
     //--------------------------------------------------------------
 
-    // Reset rotation state to prevent poisoning from temp windows
-    CORE.Window.rotation_source = (Rectangle){0};
-    CORE.Window.rotation_destination = (Rectangle){0};
-
     CORE.Window.ready = false;
     TRACELOG(LOG_INFO, "Window closed successfully");
 }
